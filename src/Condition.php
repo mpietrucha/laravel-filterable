410525b177ef4c1ebe755ab174aa31ebd6490bdc
Michał Pietrucha <mpietrucha50@gmail.com>
2025-11-23 18:32:08 +0100

    wip

--- src/Condition.php
+++ src/Condition.php
@@ -8,12 +8,15 @@ use Mpietrucha\Laravel\Filterable\Contracts\ConditionInterface;
 use Mpietrucha\Laravel\Filterable\Contracts\FilterInterface;
 use Mpietrucha\Utility\Enumerable\Contracts\EnumerableInterface;
 
+/**
+ * @phpstan-import-type ConditionFiltersCollection from \Mpietrucha\Laravel\Filterable\Contracts\ConditionInterface
+ */
 class Condition extends Field implements ConditionInterface
 {
     use InteractsWithConditions, InteractsWithInput;
 
     /**
-     * @var null|\Mpietrucha\Utility\Enumerable\Contracts\EnumerableInterface<int, \Mpietrucha\Laravel\Filterable\Contracts\FilterInterface>
+     * @var null|ConditionFiltersCollection
      */
     protected ?EnumerableInterface $filters = null;
 
--- src/Contracts/ConditionInterface.php
+++ src/Contracts/ConditionInterface.php
@@ -4,10 +4,13 @@ namespace Mpietrucha\Laravel\Filterable\Contracts;
 
 use Mpietrucha\Utility\Enumerable\Contracts\EnumerableInterface;
 
+/**
+ * @phpstan-type ConditionFiltersCollection \Mpietrucha\Utility\Enumerable\Contracts\EnumerableInterface<int, \Mpietrucha\Laravel\Filterable\Contracts\FilterInterface>
+ */
 interface ConditionInterface extends FieldInterface, InteractsWithInputInterface
 {
     /**
-     * @return \Mpietrucha\Utility\Enumerable\Contracts\EnumerableInterface<int, \Mpietrucha\Laravel\Filterable\Contracts\FilterInterface>
+     * @return ConditionFiltersCollection
      */
     public function filters(): EnumerableInterface;
 }
